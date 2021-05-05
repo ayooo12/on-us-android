bd2afa0fe4a99029375478cbc38402d535951010
Ji Young <slpjenny@naver.com>
2021-05-05 19:31:25 +0900

    Create MyPage_Fragment

---
+++ app/src/main/java/com/google/sample/cloudvision/MyPage_Fragment.java
@@ -0,0 +1,57 @@
+package com.google.sample.cloudvision;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyPage_Fragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public MyPage_Fragment() {}
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MyPage_Fragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MyPage_Fragment newInstance(String param1, String param2) {
+        MyPage_Fragment fragment = new MyPage_Fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView=(ViewGroup)inflater.inflate(R.layout.fragment_my_page_, container, false);
+
+
+
+        return rootView;
+    }
+}
