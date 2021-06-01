200a69d16feaf8d047acb7d6c6ea1188daaa5edf
ayooo12 <zhfnd00@naver.com>
2021-06-01 22:12:02 +0900

    cameraresult

---
+++ app/src/main/java/com/google/sample/cloudvision/Camera_result.java
@@ -0,0 +1,14 @@
+package com.google.sample.cloudvision;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Camera_result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_result);
+    }
+}
