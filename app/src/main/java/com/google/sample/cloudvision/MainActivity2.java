81d595e1254632aac19d48ba52fcd2e0b1c37626
Jenny Park <slpjenny@naver.com>
2021-08-11 17:19:03 +0900

    Create <성분 정보 화면> without Icon

--- app/src/main/java/com/google/sample/cloudvision/MainActivity2.java
+++ app/src/main/java/com/google/sample/cloudvision/MainActivity2.java
@@ -2,13 +2,37 @@ package com.google.sample.cloudvision;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private Button btn_camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        btn_camera = findViewById(R.id.button_camera);
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(cameraIntent);
+            }
+        });
+
     }
 }
