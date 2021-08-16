9d3a96d647996ba18a6a68163d906a524a385055
ayooo12 <zhfnd00@naver.com>
2021-08-16 20:54:46 +0900

    결과창

---
+++ app/src/main/java/com/google/sample/cloudvision/Camera_result.java
@@ -0,0 +1,14 @@
+package com.google.sample.cloudvision;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Camera_result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_result);
+    }
+}
