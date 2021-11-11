881c6c65d28deabab246928f94817e1dadb9e367
ayooo12 <zhfnd00@naver.com>
2021-11-11 22:40:54 +0900

    mypage

---
+++ app/src/main/java/com/google/sample/cloudvision/Mypage.java
@@ -0,0 +1,14 @@
+package com.google.sample.cloudvision;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mypage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypage);
+    }
+}
